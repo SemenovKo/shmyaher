de6c7ea541f61dc3cdb0609c90bb14e2688b8fa7
SemenovKo <ksem2005@gmail.com>
2024-10-31 20:00:22 +0300

    Update task1.py

--- Лаб. работа 2/task1.py
+++ Лаб. работа 2/task1.py
@@ -3,11 +3,15 @@ salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 increase = 0.05  # Ежемесячный рост цен
 budget = money_capital + salary
-# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
+
 month = 0
-while budget >= spend:
-    budget += salary
+while True:
+    difference = spend - salary
+    if difference > money_capital:
+        break
     spend += spend * increase
-    budget = budget - spend
+    money_capital -= difference
     month += 1
+
 print("Количество месяцев, которое можно протянуть без долгов:", month)
+print(f'Количество оставшихся денег под конец безбедного периода: {money_capital % difference:.2f}')
